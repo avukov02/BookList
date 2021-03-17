c762637f09b62b495e5e1ae887576e1f9b121b45
avukov02 <avukov02@fesb.hr>
2021-03-17 10:33:59 +0100

    Add project files.

---
+++ BookList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookList.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+    }
+}
